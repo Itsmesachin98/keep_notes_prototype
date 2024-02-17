e042c2f7594bd1b75f784adc9f00d34a02c8cb13
Sachin Kumar <sachinkumarskpog@gmail.com>
2024-02-17 11:19:39 +0530

    created keep notes prototype

---
+++ script.js
@@ -0,0 +1,8 @@
+// const notes = document.querySelectorAll(".note");
+
+// // let isExpand = false;
+// for (let note of notes) {
+//     note.addEventListener("click", () => {
+//         note.classList.add("overlay");
+//     });
+// }
