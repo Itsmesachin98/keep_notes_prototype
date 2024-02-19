a57b7f0e885c9d945b43505de62ff4e4431e9851
Sachin Kumar <sachinkumarskpog@gmail.com>
2024-02-19 19:53:09 +0530

    add some features

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const noteBox = document.querySelector(".note-box");
 const noteText = document.querySelector(".note-text");
+const card = document.querySelector(".card");
+const titleNoteContainer = document.querySelector(".title-note-container");
+const title = document.querySelector(".title");
+const note = document.querySelector(".note");
 
 noteText.addEventListener("click", () => {
     if (noteBox.children.length === 1) {
@@ -34,4 +38,35 @@ const createButton = () => {
     button.innerText = "Close";
     div.appendChild(button);
     noteBox.appendChild(div);
+};
+
+titleNoteContainer.addEventListener("click", () => {
+    if (card.children.length === 1) {
+        card.classList.add("maximize");
+        title.style.overflowY = "auto";
+        note.style.overflowY = "auto";
+        const div = document.createElement("div");
+        div.classList.add("close-btn-container");
+        div.appendChild(createCloseButton());
+        card.appendChild(div);
+    }
+    clickCloseButton();
+});
+
+const createCloseButton = () => {
+    const button = document.createElement("button");
+    button.classList.add("close-btn");
+    button.innerText = "Close";
+    return button;
+};
+
+const clickCloseButton = () => {
+    const closeBtnContainer = document.querySelector(".close-btn-container");
+    const closeBtn = document.querySelector(".close-btn");
+    closeBtn.addEventListener("click", () => {
+        card.classList.remove("maximize");
+        title.style.overflowY = "hidden";
+        note.style.overflowY = "hidden";
+        closeBtnContainer.remove();
+    });
 };
